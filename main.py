b79bce96b73b49ec175cea37e9db4a640167d247
Lejenay <110459738+Lejenay@users.noreply.github.com>
2023-10-07 15:57:35 +0900

    added next boss time

--- main.py
+++ main.py
@@ -16,22 +16,59 @@ jp_weekday = ["月", "火", "水", "木", "金", "土", "日"]
 boss_schedule = {
     '01:29': {'月': 'クザカ/ヌーベル', '火': 'クザカ/ヌーベル', '水': 'クザカ/ヌーベル', '木': 'カランダ/クザカ', '金': 'カランダ/クザカ', '土': 'ガーモス', '日': 'ガーモス'},
     '10:59': {'月': 'クザカ/カランダ', '火': 'クザカ/ヌーベル', '水': None, '木': 'クザカ/クツム', '金': 'クザカ/ヌーベル', '土': 'クザカ/ヌーベル', '日': 'クツム/カランダ'},
-    '13:59': {'月': 'ガーモス', '火': 'ガーモス', '水': 'ガーモス', '木': 'ガーモス', '金': 'ガーモス', '土': 'オピン', '日': None},
+    '13:59': {'月': 'ガーモス', '火': 'ガーモス', '水': 'ガーモス', '木': 'ガーモス', '金': 'ガーモス', '土': 'オピン', '日': 'ベル'},
     '15:59': {'月': 'クツム/クザカ', '火': 'クツム/クザカ', '水': 'クツム/ヌーベル', '木': 'クツム/ヌーベル', '金': 'クツム/ヌーベル', '土': 'クツム/カランダ', '日': 'クツム/カランダ'},
     '18:59': {'月': 'カランダ/ヌーベル', '火': 'クツム/ヌーベル', '水': 'カランダ/クザカ', '木': 'ガーモス', '金': 'ギュント/ムラカ', '土': 'ガーモス', '日': 'ガーモス'},
     '22:29': {'月': 'ガーモス', '火': 'ガーモス', '水': 'ガーモス', '木': None, '金': 'ガーモス', '土': None, '日': 'ギュント/ムラカ'},
     '22:59': {'月': 'クツム/カランダ', '火': 'カランダ/ヌーベル', '水': 'オピン', '木': 'ベル', '金': 'クツム/カランダ', '土': None, '日': 'オピン'},
-    '10:41': {'土': 'テストボス'}
+    '15:48': {'土': 'テストボス'}
 }
 
+def add_one_minute(time_str):
+    """
+    plus one minute to string formatted time
+    """
+
+    time_format = '%H:%M'
+    dt_obj = datetime.strptime(time_str, time_format)
+    dt_obj += timedelta(minutes=1)
+    return dt_obj.strftime(time_format)
+
+
 def find_next_boss(current_time, current_day_jp):
+    """
+    find next boss name and the spawn time of it
+    
+    Parameters
+    ----------
+    current_time : str 
+        datetime.now(JST).strftime('%H:%M') ex: '23:21'
+    current_day_jp : str
+        day name in Ja
+
+    returns
+    -------
+    next_boss_name : str
+        the name of a boss spawn next in Ja
+    next_boss_time : str
+        the spawn time of upcoming boss
+    """
+
     sorted_schedule = sorted(boss_schedule.items())
+
     for i, (time, bosses) in enumerate(sorted_schedule):
+
         if time > current_time and bosses.get(current_day_jp):
-            return bosses.get(current_day_jp)
+            return bosses.get(current_day_jp), add_one_minute(time)
+        
         elif time == current_time and bosses.get(current_day_jp):
-            return sorted_schedule[(i + 1) % len(sorted_schedule)][1].get(current_day_jp)
-    return sorted_schedule[0][1].get(current_day_jp)
+            if current_time == '18:59' and current_day_jp == '土':
+                return 'ガーモス', '01:30'
+            elif sorted_schedule[(i + 1) % len(sorted_schedule)][1].get(current_day_jp) == None:
+                return sorted_schedule[(i + 2) % len(sorted_schedule)][1].get(current_day_jp), add_one_minute(sorted_schedule[(i + 2) % len(sorted_schedule)][0])
+            return sorted_schedule[(i + 1) % len(sorted_schedule)][1].get(current_day_jp), add_one_minute(sorted_schedule[(i + 1) % len(sorted_schedule)][0])
+        
+    return sorted_schedule[0][1].get(current_day_jp), add_one_minute([k for k, v in boss_schedule.items()][0])
 
 @tasks.loop(seconds=30)
 async def send_message_at_time():
@@ -43,20 +80,21 @@ async def send_message_at_time():
     try:
         if current_time in boss_schedule:
             boss_name = boss_schedule[current_time].get(current_day_jp)
-            next_boss = find_next_boss(current_time, current_day_jp)
+            next_boss, next_boss_time = find_next_boss(current_time, current_day_jp)
             if boss_name:
                 channel = bot.get_channel(int(CHANNEL_ID))
 
                 embed = discord.Embed(
                     title="ボス出現アラート",
-                    description=f"まもなく **{boss_name}** が出現します",
-                    color=0x75ffd3,
+                    description=f"⚡\nまもなく **{boss_name}** が出現します",
+                    color=0x00bf5c,
                     url="https://garmoth.com/boss-timer",
                     timestamp=datetime.now(timezone.utc)
                 )
                 
                 if next_boss:
                     embed.add_field(name="次のボス", value=next_boss)
+                    embed.add_field(name="時間", value=next_boss_time)
                 embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1159883552494735513/1160028857810821150/pngaaa.com-3957460.png?ex=65332c0f&is=6520b70f&hm=ed2a27f6e6655200a4ce62dfa80699a4789e2d46e4e6691841c76e24ee33ab30&")
                 embed.set_footer(text="Powered by Lejenay",
                      icon_url="https://cdn.discordapp.com/attachments/284247077771411456/1160019204129431662/ico_lejenay_45_512x.png?ex=65332312&is=6520ae12&hm=8e0a74a9ffc6d87660acced35640a43de66584b07340fac1a6db90c7923e41fd&")
